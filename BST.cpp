b08f51ea12abb4c341489d6d1b137a89f3c121f7
Abdallah Hussein <abiza2013@gmail.com>
2023-05-08 04:12:19 +0300

    Delete Student BST

--- BST.cpp
+++ BST.cpp
@@ -9,14 +9,28 @@ private:
     BST *left;
     BST *right;
     bool searchStudentHelper(int ID);
+    BST *minNode()
+    {
+        BST *current = this;
+        while (current && current->left)
+            current = current->left;
+        return current;
+    }
+    BST *deleteNode(int ID, BST *node);
 
 public:
     BST(Student student) : student(student), left(nullptr), right(nullptr) {}
     void addStudent(Student student);
     void searchStudent(int ID);
+    void deleteStudent(int ID);
 };
 void BST::addStudent(Student student)
 {
+    if (student.ID < 0 || student.ID > 100)
+    {
+        cout << "ID should be between 0 and 100\n";
+        return;
+    }
     if (student.ID < this->student.ID)
     {
         if (!left) // if there is no left student (left == nullptr)
@@ -64,12 +78,70 @@ bool BST::searchStudentHelper(int ID)
     }
     return (right && right->searchStudentHelper(ID));
 }
+BST *BST::deleteNode(int ID, BST *node)
+{
+    if (!node) // There is no node
+        return nullptr;
+    if (ID > node->student.ID) // If the ID is in the right subtree
+    {
+        node->right = deleteNode(ID, node->right);
+    }
+    else if (ID < node->student.ID) // If the ID is in the left subtree
+    {
+        node->left = deleteNode(ID, node->left);
+    }
+    else // if ID is found (node is found) (==)
+    {
+        if (!node->left && !node->right) // Case: 1 if there is no left/right subtree (has no child)
+        {
+            node = nullptr;
+        }
+        // Case: 2 if there is only one child
+        else if (!node->left) // If there is no left
+        {
+            BST *temp = node->right;
+            delete node;
+            node = temp;
+        }
+        else if (!node->right) // If there is no right
+        {
+            BST *temp = node->left;
+            delete node;
+            node = temp;
+        }
+        // Case: 3 if there is 2 child then use successor
+        else
+        {
+            BST *min = node->right->minNode();
+            node->student.ID = min->student.ID;
+            node->student.name = min->student.name;
+            node->student.department = min->student.department;
+            node->student.GPA = min->student.GPA;
+            node->right = deleteNode(node->student.ID, node->right);
+        }
+    }
+    return node;
+}
+
+void BST::deleteStudent(int ID)
+{
+    if (this->student.ID == ID && !this->left && !this->right) // if it's the root then do nothing
+    {
+        cout << "Can't delete the root\n";
+        return;
+    }
+    deleteNode(ID, this);
+}
 
 int main()
 {
-    BST bst(Student(0, "Abdo", "CS", 4.441));
-    bst.addStudent(Student(1, "Boda", "IS", 4.442));
-    bst.addStudent(Student(2, "7mada", "IT", 4.443));
+    BST bst(Student(50, "Abdo", "CS", 4.441));
+    bst.addStudent(Student(5, "Boda", "IS", 4.442));
+    bst.addStudent(Student(90, "7mada", "IT", 4.443));
+    bst.addStudent(Student(6, "Body", "IP", 4.444));
 
-    bst.searchStudent(2);
+    bst.searchStudent(90);
+    bst.deleteStudent(50);
+    // bst.searchStudent(90);
+    // bst.searchStudent(1);
 }
